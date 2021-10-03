1ecdabf92e666d1be6d2d016bce08aaf7d4d3c8f
cker90@163.com <cker90!qaz2wsx>
2021-10-03 21:52:05 +0800

    projects

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using StaticLibrary;
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            int sum = MathOperation.Add(num1, num2); // Method from class library    
+            Console.WriteLine($"{num1} + {num2} = {sum}");
+            Console.ReadLine();
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ StaticLibrary/MathOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StaticLibrary
+{
+   
+    // 数学运算静态类
+   public static class MathOperation    
+    {    
+        // 这是一个两个数字的简单加法的方法
+        public static int Add(int num1, int num2) => num1 + num2;
+    }  
+}
