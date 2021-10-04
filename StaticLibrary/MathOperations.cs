e516ef405f75529ec63e231b21d3ce08f4d55549
cker90@163.com <cker90!qaz2wsx>
2021-10-04 11:20:17 +0800

    静态类库  数学运算类 新增其他运算方法

--- StaticLibrary/MathOperations.cs
+++ StaticLibrary/MathOperations.cs
@@ -2,11 +2,17 @@
 
 namespace StaticLibrary
 {
-   
+
     // 数学运算静态类
-   public static class MathOperation    
-    {    
+    public static class MathOperation
+    {
         // 这是一个两个数字的简单加法的方法
         public static int Add(int num1, int num2) => num1 + num2;
-    }  
+        //这是一个两个数字的简单减法的方法
+        public static int Subtract(int num1, int num2) => num1 - num2;
+        //这是一个两个数字的简单乘法的方法
+        public static int Multiply(int num1, int num2) => num1 * num2;
+        //这是一个两个数字的简单除法的方法
+        public static int Divide(int num1, int num2) => num1 / num2;
+    }
 }
