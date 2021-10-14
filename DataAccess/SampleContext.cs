e5f5dcceaa9adb31ab9fc2385267be43ddafa02c
cker90@163.com <cker90!qaz2wsx>
2021-10-14 15:12:35 +0800

    使用service

--- DataAccess/SampleContext.cs
+++ DataAccess/SampleContext.cs
@@ -6,10 +6,10 @@ using System.Linq;
 using System.Text;
 namespace DataAccess
 {
-     public class ProductContext : DbContext
+     public class SampleContext : DbContext
     {
         //https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/complex-data-model
-        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        public SampleContext(DbContextOptions<SampleContext> options) : base(options)
         {
             //在此可对数据库连接字符串做加解密操作
         }
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -8,6 +8,11 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DataAccess;
+using Services.ProductService;
+using Microsoft.EntityFrameworkCore;
+using Entity.Table;
+using Arch.EntityFrameworkCore.UnitOfWork;
 
 namespace Web
 {
@@ -23,6 +28,13 @@ namespace Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SampleContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("MySqlConnection")));//添加Mysql支持
+
+            services.AddUnitOfWork<SampleContext>();//添加UnitOfWork支持
+            services.AddScoped(typeof(IProductService), typeof(ProductService));//用ASP.NET Core自带依赖注入(DI)注入使用的类
+            
+
             services.AddControllersWithViews();
         }
 
